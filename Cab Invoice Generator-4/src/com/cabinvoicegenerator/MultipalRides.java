5e895c096556a5f5acae01468794d36ef31c3268
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-05-05 14:58:00 +0530

    UC4

---
+++ Cab Invoice Generator-4/src/com/cabinvoicegenerator/MultipalRides.java
@@ -0,0 +1,14 @@
+package com.cabinvoicegenerator;
+
+public class MultipalRides {
+	public MultipalRides(double d, int i) {
+	}
+
+	public double getDistance() {
+		return 0;
+	}
+
+	public int getTime() {
+		return 0;
+	}
+}
